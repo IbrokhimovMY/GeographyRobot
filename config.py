0c0274e3a550eaedfdb21fcbea1254bcd38178fd
ibrokhimovmy <ibrokhimovmy@gmail.com>
2026-05-27 03:43:46 +0200

    Add `.gitignore` for idea files and implement find_countries.py with full country and capital lists in Uzbek

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN: str = os.getenv('BOT_TOKEN', '8871176388:AAFyjzThmAC_MWpvGzAyK-gJPt-UXQ6kgN4')
+BOT_TOKEN: str = os.getenv('BOT_TOKEN', '')
 WEBAPP_URL: str = os.getenv('WEBAPP_URL', '')
 DB_PATH: str = os.getenv('DB_PATH', 'geography_bot.db')
 SUPPORTED_LANGS: tuple = ('uz', 'ru', 'en')
